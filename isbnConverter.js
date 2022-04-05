41b60b745b9dadd5171013666c0b588c06b33970
phaydara <phaydara@gmail.com>
2022-04-05 17:50:20 -0500

    alternately multiply each digit from L to R by 1 or 3

--- isbnConverter.js
+++ isbnConverter.js
@@ -4,17 +4,23 @@ const isbnConverter = isbn => {
     let isbnArr = isbn.split(''); 
     // console.log(isbnArr); // ['1', '-', '8', '5','3', '2', '6', '-','1', '5', '8', '-', '0']
     isbnArr.pop(); // ['0']
-    // console.log(isbnArr); // ['1', '-', '8', '5','3', '2', '6', '-','1', '5', '8', '-']
+    // console.log(isbnArr); // ['1', '-', '8', '5','3', '2', '6', '-', '1', '5', '8', '-']
 
     // add prefix: 978
-    isbnArr.unshift('978', '-')
-    // console.log(isbnArr); // ['978', '-', '1', '-', '8', '5','3', '2', '6', '-','1', '5', '8', '-']
+    isbnArr.unshift('9', '7', '8', '-')
+    // console.log(isbnArr); // ['9', '7', '8', '-', '1', '-', '8', '5', '3', '2', '6', '-','1', '5', '8', '-']
 
     // convert to 12 digits
-    isbnArr = isbnArr.filter(char => char !== '-');
-    // console.log(isbnArr.filter(char => char !== '-').join('')); // 978185326158
+    isbnArr = isbnArr.filter(char => char !== '-'); 
+    console.log(isbnArr.filter(char => char !== '-')); // ['9', '7', '8', '1', '8', '5', '3', '2', '6', '1', '5', '8']
 
-    return isbnArr.join('');
+    // alternately multiply each digit from L to R by 1 or 3
+    // even index * 1
+    // odd index * 3
+    isbnArr = isbnArr.map((v, k) => (k % 2) ? v * 3 : v * 1); 
+    console.log(isbnArr); // [9, 21, 8, 3, 8, 15, 3, 6, 6, 3, 5, 24]
+    
+    return isbnArr;
 };
 
 console.log(isbnConverter('1-85326-158-0')); // 978-1-85326-158-9
