0d7c0fd68c445c8744290a046fce305850053b4f
phaydara <phaydara@gmail.com>
2022-04-05 17:26:41 -0500

    removed last character of isbn

--- isbnConverter.js
+++ isbnConverter.js
@@ -1,15 +1,15 @@
 const isbnConverter = isbn => {
 
     // remove last character of isbn
-    let isbnArr = isbn.split('');
-    console.log(isbnArr);
-    isbnArr.pop();
-    console.log(isbnArr);
-
-    // add prefix: 978
-
+    let isbnArr = isbn.split(''); 
+    // console.log(isbnArr); // ['1', '-', '8', '5','3', '2', '6', '-','1', '5', '8', '-', '0']
+    isbnArr.pop(); // ['0']
+    // console.log(isbnArr); // ['1', '-', '8', '5','3', '2', '6', '-','1', '5', '8', '-']
 
     return isbnArr.join('');
 };
 
-console.log(isbnConverter('1-85326-158-0'));
+console.log(isbnConverter('1-85326-158-0')); // 978-1-85326-158-9
+// console.log(isbnConverter('0-14-143951-3')); // 978-0-14-143951-8
+// console.log(isbnConverter('0-02-346450-X')); // 978-0-02-346450-8
+// console.log(isbnConverter('963-14-2164-3')); // 978-963-14-2164-4
